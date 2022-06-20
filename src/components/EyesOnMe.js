79aa12a855cb3b8e2b828e9987636623bf1bec9a
Joshua Welsh <j22yosh@gmail.com>
2022-06-19 20:08:20 -0400

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+
+    function log() {console.log("Good!") }
+    function log1() {console.log('Hey! Eyes on me!') }
+ return (<button onFocus={log} onBlur={log1}>Eyes on me</button>)
+    
+}
+
+export default EyesOnMe
